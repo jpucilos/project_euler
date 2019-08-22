b2e614f8c394151db35cefda73baa3c4949a4cae
jpucilos <jpucilos@stevens.edu>
2019-08-21 21:47:27 -0400

    p021

--- p021.cpp
+++ p021.cpp
@@ -5,11 +5,10 @@ using namespace std;
 #define MAX 10000
 
 int proper_divisor_sum (int i){
-    int sum = 0;
-    for (int n = 1; n <= sqrt(i); n++){
+    int sum = 1;
+    for (int n = 2; n <= i / 2; n++){
         if( i % n == 0){
             sum += n;
-            sum += i / n;
         }
     }
     return sum;   
@@ -21,31 +20,14 @@ int main()
     bool amicable[MAX];
     int proper_divisor_sums[MAX];
     int result = 0;
-    
-    for (int i = 0; i < MAX; i++){
-        amicable[i] = false;
-        proper_divisor_sums[i] = 0;
-    }
-    
-    
-    for (int i = 2; i < MAX; i+=2){
-        if(amicable[i] == false){
-            if(proper_divisor_sums[i] == 0){
-                proper_divisor_sums[i] = proper_divisor_sum(i);
-                proper_divisor_sums[proper_divisor_sums[i]] = proper_divisor_sum(proper_divisor_sums[i]);
-                
-            }
-            if(i == proper_divisor_sums[proper_divisor_sums[i]]){
-                amicable[i] = true;
-                amicable[proper_divisor_sums[i]] = true;
-                cout << "Amicable Number: " << i << '\n';
-                result += i;
-            }
-        }
-        else
-            cout << "Amicable Number: " << i << '\n';
-            result += i;
-        
-    }
+    for (int i  = 2; i < MAX; i++){
+		if (proper_divisor_sum(proper_divisor_sum(i)) ==i){
+			if ( proper_divisor_sum(i) != i){
+				result += i;
+			}
+			
+		}
+	}
+		
     cout << result<<'\n';
 }
